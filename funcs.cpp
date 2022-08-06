03ea10e162be1f42e12eb6bd81bcd78cd6ab22cd
johnzhou1210 <john.zhou81@myhunter.cuny.edu>
2022-08-06 14:34:57 -0400

    finished process input

--- funcs.cpp
+++ funcs.cpp
@@ -1,4 +1,25 @@
 #include "funcs.h"
-vector<vector<int>> process_input(){
-    freopen(INPUT, "r", stdin);
+
+vector<vector<int>> solver::process_input(){
+    // take in input
+    std::fstream newFile;
+    
+    vector<vector<int>> result;
+    newFile.open("input.txt", std::ios::in);
+    if (newFile.is_open()) {
+        string s;
+        while(std::getline(newFile, s)) {
+            vector<int> subResult;
+            for(int i = 0; i < s.length(); i++) {
+                if (s[i] != ',') {
+                    //subResult.push_back(std::stoi((std::to_string(s[i] - 48)))); other way
+                    subResult.push_back(s[i] - 48);
+                }
+            }
+            result.push_back(subResult);
+        }
+        newFile.close();
+    }
+   
+    return result;
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include "funcs.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+using std::cout;
+
+
 
 int main(){
-
+    solver slvr;
+    vector<vector<int>> input = slvr.process_input();
+    
+    for( int i =0 ; i < 9; i ++) {
+        for (int j = 0; j < 9; j++) {
+            cout << input[i][j] << " ";
+        }
+        cout << "\n";
+    }
     return 0;
 }
+
